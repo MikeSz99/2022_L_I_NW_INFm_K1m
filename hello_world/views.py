8a357072345678cb854fa0d34e8bb3a130c8b80d
MikeSz99 <michal.szymonik99@gmail.com>
2022-10-30 17:29:42 +0100

    Poprawiono bledy

--- hello_world/views.py
+++ hello_world/views.py
@@ -6,7 +6,6 @@ from flask import request
 moje_imie = "Michal"
 msg = "Hello World!"
 
-
 @app.route('/')
 def index():
     output = request.args.get('output')
@@ -15,8 +14,6 @@ def index():
     return get_formatted(msg, moje_imie,
                          output.lower())
 
-
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
-
